f919a58c0de35fe684b2b44d8f40bb44ab4faf17
Catalina Ubilla Rubio <cataubilla@MacBook-Pro-de-Catalina.local>
2023-02-12 15:23:24 -0300

    Actualización 12.02.23

--- flask_crud/utils/utils.py
+++ flask_crud/utils/utils.py
@@ -5,7 +5,7 @@ def allowed_file(filename):
     return '.' in filename and \
         filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/Users/cataubilla/Desktop/Escritorio_cataubilla/coding_dojo/proyectos/csv_to_graphic/uploads/<name>')
+@app.route('/Users/cataubilla/Desktop/Escritorio_cataubilla/PROGRAMACION/coding_dojo/proyectos/csv_to_graphic/uploads/<name>')
 def download_file(name):
     return send_from_directory(app.config["UPLOAD_FOLDER"], name)
         
